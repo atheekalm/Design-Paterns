4ba6ca02ed207e1a019cb674c7c17f417cfe0f6a
Atheek <55016532+atheekalm@users.noreply.github.com>
2021-01-08 01:30:32 +0530

    Add files via upload

---
+++ Policy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Patern
+{
+    public class Policy
+    {
+        private static Policy _instance;
+
+        public static Policy Instance 
+        { 
+            get
+            {
+                if (_instance==null)
+                {
+                    _instance = new Policy();
+                }
+                return _instance;
+            } 
+        }
+        public int Id { get; set; }
+        private string Insured { get; set; } = "Atheek Ahammed";
+        public string GetInsured() => Insured;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Patern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var policy = new Policy();
+            var InsuredName = Policy.Instance.GetInsured();
+            Console.WriteLine(InsuredName);
+        }
+    }
+}
